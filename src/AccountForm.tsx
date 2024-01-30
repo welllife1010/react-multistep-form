e403df219b82c9a2317ae8cf259d90a04d9d688e
Silvia Chen <silviachen@Silvia-Chens-MacBook-Pro-1292.local>
2024-01-29 20:02:19 -0800

    adding states & state updates mechanism - part 2

--- src/AccountForm.tsx
+++ src/AccountForm.tsx
@@ -1,12 +1,36 @@
 import { FormWrapper } from "./FormWrapper"
 
-export function AccountForm() {
+type AccountData = {
+  email: string
+  password: string
+}
+
+type AccountDataProps = AccountData & {
+  updateFields: (fields: Partial<AccountData>) => void
+}
+
+export function AccountForm({
+  email,
+  password,
+  updateFields,
+}: AccountDataProps) {
   return (
     <FormWrapper title="Accont Creation">
       <label>Email</label>
-      <input autoFocus required type="text" />
+      <input
+        autoFocus
+        required
+        type="text"
+        value={email}
+        onChange={(e) => updateFields({ email: e.target.value })}
+      />
       <label>Password</label>
-      <input required type="password" />
+      <input
+        required
+        type="password"
+        value={password}
+        onChange={(e) => updateFields({ password: e.target.value })}
+      />
     </FormWrapper>
   )
 }
