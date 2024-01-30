92d2bfd9cd7ca9fd517f30f0bee9be341d9f67e9
Silvia Chen <silviachen@Silvia-Chens-MacBook-Pro-1292.local>
2024-01-29 17:47:26 -0800

    adding states & state updates mechanism - part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
+import { useState } from "react"
 import { AccountForm } from "./AccountForm"
 import { AddressForm } from "./AddressForm"
 import { UserForm } from "./UserForm"
 import { useMultistepForm } from "./useMultistepForm"
 
+type FormData = {
+  firstName: string
+  lastName: string
+  age: string
+  street: string
+  city: string
+  state: string
+  zip: string
+  email: string
+  password: string
+}
+
+const INITIAL_DATA: FormData = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+  email: "",
+  password: "",
+}
+
 function App() {
+  const [data, setData] = useState(INITIAL_DATA)
+  function updateFields(fields: Partial<FormData>) {
+    setData((prev) => {
+      return { ...prev, ...fields }
+    })
+  }
   const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
-    useMultistepForm([<UserForm />, <AddressForm />, <AccountForm />])
+    useMultistepForm([
+      <UserForm {...data} updateFields={updateFields} />,
+      <AddressForm {...data} updateFields={updateFields} />,
+      <AccountForm {...data} updateFields={updateFields} />,
+    ])
 
   function onSubmit(e: FormEvent) {
     e.preventDefault()
